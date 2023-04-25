b13cd486545ca1b1d02d3cdd616582d675502711
jlebre <josemariamlebre@gmail.com>
2023-04-25 17:17:04 +0100

    Update

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlebre <jlebre@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 20:27:39 by jlebre            #+#    #+#             */
-/*   Updated: 2023/01/04 20:27:40 by jlebre           ###   ########.fr       */
+/*   Updated: 2023/04/25 17:16:36 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,6 @@ int main()
 		}
 		else if (!input.compare("EXIT"))
 			break ;
-		else if (!input.compare("CALL"))
-			yellowpages.call(len);
 		else if (!input.empty())
 		{
 			std::cout << "\n\033[0;31mINVALID COMMAND!\033[0m" << std::endl;
