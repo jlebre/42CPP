25f9ecd17d842f8b3f32e40c3382d91da979b047
jlebre <josemariamlebre@gmail.com>
2023-06-06 13:17:09 +0100

    Update

---
+++ CPP03/ex02/srcs/ScavTrap.cpp
@@ -0,0 +1,38 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap( const std::string name )
+{
+	_name = name;
+	_hit = 100;
+	_energy = 50;
+	_attack = 20;
+	std::cout << "Scav Constructor Called\n";
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "Scav Destructor Called\n";
+}
+
+void    ScavTrap::attack( const std::string &target )
+{
+	if (_hit <= 0)
+	{
+		std::cout << _name << " is dead.\n";
+		return ;
+	}
+	if (_energy <= 0)
+	{
+		std::cout << "Energy too low...\n";
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " attacks " \
+		<< target << ", causing " << _attack \
+		<< " point of damage!\n";
+	this->_energy--;
+}
+
+void	ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << _name << " is now in Gate keeper mode\n";
+}
